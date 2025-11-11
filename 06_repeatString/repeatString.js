18ed9bc7aa2fbdf0166b3cc95553c77694123ce7
Aidan Walny <222332094+aidan-walny@users.noreply.github.com>
2025-11-11 18:58:08 -0500

    SPEC: remove skips to run all tests.
    JS:
    fixed return string value.
    added a line to return error if < 0.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,12 +1,14 @@
 const repeatString = function(string, increment) {
     let counter = 0
     let newWord = ''
+    if (increment < 0) return 'ERROR';
     while (counter < increment){
         newWord += string;
         counter += 1;
     }
-    return string;
+    return newWord;
 
 };
+// repeatString('hey', 3);
 // Do not edit bel                                                                                                                              ``````++++++++++++++ow this line
 module.exports = repeatString;
