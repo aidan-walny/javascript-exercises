584bb328dcadfd939b0916b6e80483cf3f6b42b4
Aidan Walny <222332094+aidan-walny@users.noreply.github.com>
2025-11-11 18:52:59 -0500

    saving function to repeat string.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, increment) {
+    let counter = 0
+    let newWord = ''
+    while (counter < increment){
+        newWord += string;
+        counter += 1;
+    }
+    return string;
 
 };
-
-// Do not edit below this line
+// Do not edit bel                                                                                                                              ``````++++++++++++++ow this line
 module.exports = repeatString;
