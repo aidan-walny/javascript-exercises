ed421be1ca1430d141d03d5c269ed11702625c48
Aidan Walny <222332094+aidan-walny@users.noreply.github.com>
2025-11-13 18:16:23 -0500

    JS: build function for first test.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(passed_array, passed_removable) {
+    console.log(passed_array);
+    removableIndex = passed_array.indexOf(passed_removable, 0)
+    // console.log(removableIndex);
+    passed_array.splice(removableIndex, 1);
+    return passed_array;
+    // console.log(passed_array);
+
 };
 
+// removeFromArray([1, 2, 3, 4], 3)
 // Do not edit below this line
 module.exports = removeFromArray;
