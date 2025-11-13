b483c79e30138fb4e8cabac189f784cc348a2070
Aidan Walny <222332094+aidan-walny@users.noreply.github.com>
2025-11-13 17:55:53 -0500

    SPEC: remove skips.
    JS:
    build function to reverse string.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(passed_string) {
+    let newString = ''
+    newString = passed_string.split('').reverse().join('');
+    return newString
 };
 
+reverseString("test")
 // Do not edit below this line
 module.exports = reverseString;
