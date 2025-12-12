abca520c7b0af06561226202bf94c1264134c5c0
Aidan Walny <222332094+aidan-walny@users.noreply.github.com>
2025-12-11 19:28:29 -0500

    JS: Write function to convert fahrenheit to celsius, and celsius to fahrenheit.
    SPEC:
    Modify file to run tests.

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(passedFahrenheit) {
+
+  celsiusValue =  (passedFahrenheit - 32) * 5 / 9;
+  return +celsiusValue.toFixed(1);
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(passedCelsius) {
+
+  fahrenheitValue = (passedCelsius * 1.8) + 32
+  return +fahrenheitValue.toFixed(1);
 };
 
 // Do not edit below this line
