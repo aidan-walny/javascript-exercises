7176db22b38c3d47ce058361bfed22674b08b89d
Aidan Walny <222332094+aidan-walny@users.noreply.github.com>
2025-12-11 19:15:20 -0500

    JS: Save function to tell true or false for a leap year value.
    SPEC:
    Remove the skip for tests.

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
+const leapYears = function(passedYear) {
 
+    if (passedYear % 4 === 0 && (!(passedYear % 100 === 0) || (passedYear % 400 === 0)))   {
+        return true;
+    }
+
+    else return false;
 };
 
+value = leapYears(2000)
+console.log (value)
 // Do not edit below this line
 module.exports = leapYears;
